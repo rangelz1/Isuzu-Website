bf96899fe196a928aca1f04d9502a2349595aef6
rangelz1 <rangelz187@gmail.com>
2017-04-18 16:55:28 -0700

    Initial animation css

---
+++ js/main.js
@@ -0,0 +1,4 @@
+function myDelay() {
+    document.getElementById("indxh1").style.WebkitAnimationDelay = "2s";  // Code for Chrome, Safari, and Opera
+    document.getElementById("indxh1").style.animationDelay = "2s";
+}

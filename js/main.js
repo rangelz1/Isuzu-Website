fe072864c49807e53d864c4de2a1bb61fad44320
rangelz1 <rangelz187@gmail.com>
2017-04-19 11:59:02 -0700

    Integration of text annimation to content

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+
+
+(function() {
+	var aboutEl = $('div.about'),
+	aboutElOffset = 350,
+	documentEl = $(document);
+
+	documentEl.on('scroll', function() {
+		if ( documentEl.scrollTop() > aboutElOffset && aboutEl.hasClass('hidden') ) aboutEl.removeClass('hidden');
+	});
+})();
+
 function myDelay() {
     document.getElementById("indxh1").style.WebkitAnimationDelay = "2s";  // Code for Chrome, Safari, and Opera
     document.getElementById("indxh1").style.animationDelay = "2s";
